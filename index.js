905b3b7ad988094c0579326b54cfd914f256d2d5
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-09 09:53:14 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,71 @@
+// VARIABLES
+// For the navigator (person giving instructions),
+// Define three variables: `name`, `numberOfSiblings`, and `isBilingual`. Determine which should be constants and which may change.
+let name = "";
+let numberOfSiblings = 0
+let isBillingual;
+
+
+// Assign corresponding values with the most appropriate types for each
+name = "Fatoumata";
+numberOfSiblings = 7;
+isBillingual = true;
+
+
+// Print these variables to the console.
+console.log(name)
+console.log(numberOfSiblings)
+console.log(isBillingual)
+
+
+// RE-ASSIGNMENT
+// Create a variable `counter` and initialize it with 0.
+let counter = 0;
+// Set it to a larger value
+counter = 5;
+// Increment the `counter` by 1 without specifying the number!
+counter = counter + 1;
+// Print the updated value of `counter`.
+console.log(counter)
+
+// DATA TYPES
+// Create variables for various primitive data types: `number`, `string`, `boolean`, and `null`.
+
+  let num = 6;
+  let string = "Liana is having a bad day."
+  let isRaining = false;
+  let Null = null;
+
+
+
+
+// Print the values and confirm their type using typeof
+
+console.log(num)
+console.log(string)
+console.log(isRaining)
+console.log(Null)
+
+console.log(typeof num)
+console.log(typeof string)
+console.log(typeof isRaining)
+console.log(typeof Null)
+
+// Create a variable that has an undefined type 
+
+// Print the variable and confirm its type
+
+
+
+// EXTRA CREDIT (Google these!)
+// Make an array of siblings/languages that you speak
+
+
+
+// Make an object with your name, siblings, and languages
+
+
+
+// Try printing the number of siblings you have using ONLY the object (Hint: look up how to find the length of an array)
+
+
